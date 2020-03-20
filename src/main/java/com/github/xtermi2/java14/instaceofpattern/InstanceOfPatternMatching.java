4908d711a87b9afa5786ed0de4b20324fbf3c6ac
Andreas Keefer <xtermi2@users.noreply.github.com>
2020-03-20 11:20:00 +0100

    code format / cleanup

--- src/main/java/com/github/xtermi2/java14/instaceofpattern/InstanceOfPatternMatching.java
+++ src/main/java/com/github/xtermi2/java14/instaceofpattern/InstanceOfPatternMatching.java
@@ -3,9 +3,9 @@ package com.github.xtermi2.java14.instaceofpattern;
 public class InstanceOfPatternMatching {
 
     public String instaceOfTest(Object msg) {
-        if (msg instanceof String s){
+        if (msg instanceof String s) {
             return "this is the poor man's smart-cast feature from kotlin: " + s.length();
-        } else{
+        } else {
             return "";
         }
     }
